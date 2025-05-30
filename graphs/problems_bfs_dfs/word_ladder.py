88c96814581bd9e639090a2fab83c64d4799132e
Ritesh Ojha <ojharitesh1998@gmail.com>
2025-05-30 19:46:26 -0400

    word_ladder

--- graphs/problems_bfs_dfs/word_ladder.py
+++ graphs/problems_bfs_dfs/word_ladder.py
@@ -25,13 +25,17 @@ class Solution:
         while queue:
             word, level = queue.popleft()
             for i in range(L):
+                #create a pattern with the current word present in the queue
                 pattern = word[:i] + '*' + word[i+1:]
+                #check for the pattern created and words associated with it and if the word matches with the endWord
                 for neighbour in pattern_map[pattern]:
                     if neighbour == endWord:
                         return level + 1
+                    #if the word associated with the pattern is not visited, add it to the queue and mark it as visited
                     if neighbour not in visited:
                         visited.add(neighbour)
                         queue.append((neighbour, level + 1))
+                #after checking all the words associated with the pattern, clear the pattern map to avoid reprocessing
                 pattern_map[pattern] = []
         return 0
 
